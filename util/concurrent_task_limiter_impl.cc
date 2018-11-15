7cad58f7c9e808dee4a57509c4f88581ec94e203
Burton Li <pul@microsoft.com>
2018-11-15 15:41:34 -0800

    remove pragma once typo

--- util/concurrent_task_limiter_impl.cc
+++ util/concurrent_task_limiter_impl.cc
@@ -7,8 +7,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#pragma once
-
 #include "util/concurrent_task_limiter_impl.h"
 #include "rocksdb/concurrent_task_limiter.h"
 
